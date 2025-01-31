9b10152a83b2d776c0d660db426b9d509408ef59
blankcry <gajejames@gmail.com>
2025-01-31 15:25:40 +0100

    chore: finished home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+import { Icon } from "@iconify/react";
 import Blankcry from "@/assets/logo_100x40.svg";
+
 function Navbar() {
   const components: { title: string; href: string }[] = [
     {
@@ -26,14 +29,16 @@ function Navbar() {
       href: "contacts",
     },
   ];
+  const [activeSection, setActiveSection] = useState<string>("");
   const handleScroll = (id: string) => {
     const element = document.getElementById(id);
     if (element) {
+      setActiveSection(id);
       element.scrollIntoView({ behavior: "smooth" });
     }
   };
   return (
-    <nav className="bg-black dark:bg-white text-white dark:text-black flex flex-col h-screen border-r-2 border-white dark:border-black max-w-max sticky gap-8 p-4">
+    <nav className="bg-black text-white flex md:flex-col md:h-screen border-r-2 border-white max-w-max sticky gap-8 p-4 dark:border-r-2 dark:border-white">
       <a href="/" target="_blank">
         <img
           src={Blankcry}
@@ -43,16 +48,37 @@ function Navbar() {
           height={"100px"}
         />
       </a>
-      <div className="flex flex-col gap-2 text-left font-ibm uppercase">
+      <div className="flex-1 flex md:flex-col gap-2 text-left font-ibm uppercase">
         {components.map((component) => (
           <div
             key={component.title}
             onClick={() => handleScroll(component.href)}
-            className="hover:cursor-pointer hover:bg-slate-500 hover:text-white hover:font-bold px-8 py-4 rounded-md"
+            className={
+              "hover:cursor-pointer hover:bg-white hover:text-black hover:font-bold px-8 py-4 rounded-md" +
+              (activeSection === component.href
+                ? " bg-white text-black font-bold"
+                : "")
+            }
           >
             <span key={component.title}>{component.title}</span>
           </div>
         ))}
+      </div>
+      {/* Bottom Elements */}
+      <div className="flex flex-col gap-4 px-4">
+        {/* Social Links or Logout */}
+        <div className="flex flex-col gap-4 ">
+          <a href="https://facebook.com/james-yunana">
+            <Icon icon="ic:round-facebook" width="24" height="24" />
+          </a>
+          <a href="https://x.com/james_yuna">
+            <Icon icon="ri:twitter-x-line" width="24" height="24" />
+          </a>
+          <a href="https://instagram.com/james_yuna">
+            <Icon icon="ph:instagram-logo-thin" width="24" height="24" />
+          </a>
+        </div>
+        <p className="text-xs text-gray-400">© 2025 Blankcry</p>
       </div>
     </nav>
   );
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,71 @@
 import HeroImage from "@/assets/heroImage.png";
 import AppleClip from "@/assets/apple-art.svg";
+import NorthEast from "@/assets/north_east.svg";
+import { Icon } from "@iconify/react";
 
 function Home() {
+  const handleScroll = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
-    <section id="home" className="w-full p-32 flex flex-col gap-8 h-screen">
+    <section
+      id="home"
+      className="w-full p-32 flex flex-col gap-8 h-screen font-ibm"
+    >
       <div className="flex gap-4 w-full">
-        <div className="p-4 w-full">
-            <img src={AppleClip} className="-rotate-12 relative -bottom-16 -left-16"/>
-            <span className="uppercase text-[102px] leading-[90px]">my name <br/>is <span className="font-bold">james <br/> yunana...</span></span>
+        <div className="p-4 w-full flex flex-col gap-6 items-start">
+          <div>
+            <img
+              src={AppleClip}
+              className="-rotate-12 relative -bottom-16 -left-16"
+            />
+            <span className="uppercase text-[102px] leading-[90px]">
+              my name <br />
+              is{" "}
+              <span className="font-bold">
+                james <br /> yunana...
+              </span>
+            </span>
+            <br />
+            <span className="text-[32px] leading-5 tracking-tight text-nowrap flex gap-2 items-center text-left font-ibm">
+              <span className="font-bold italic capitalize">
+                software developer
+              </span>{" "}
+              based in{" "}
+              <Icon icon="game-icons:nigeria" fill="green" stroke="green" />{" "}
+            </span>
+          </div>
+          <button
+            className="dark:bg-white bg-black text-white py-3 px-6 font-medium dark:text-black flex gap-4 text-[18px] leading-6"
+            onClick={() => handleScroll("about")}
+          >
+            Connect with me <img src={NorthEast} />
+          </button>
+          <div className="flex gap-x-6 items-center">
+            <hr className="w-[120px] h-[2px] border-0 bg-black dark:bg-white" />
+            <a href="https://facebook.com/james-yunana">
+              <Icon icon="ic:round-facebook" width="24" height="24" />
+            </a>
+            <a href="https://x.com/james_yuna">
+              <Icon icon="ri:twitter-x-line" width="24" height="24" />
+            </a>
+            <a href="https://instagram.com/james_yuna">
+              <Icon icon="ph:instagram-logo-thin" width="24" height="24" />
+            </a>
+          </div>
+          <div className="flex justify-start gap-4 items-center w-full">
+            <span className="flex gap-3 items-center">
+              <Icon icon="el:phone-alt" width="24" height="24" />
+              +234 704 101 8558
+            </span>
+            <span className="flex gap-3 items-center">
+              <Icon icon="mdi:email-box" width="24" height="24" />
+              <span>gajejames@outlook.com</span>
+            </span>
+          </div>
         </div>
         <div className="w-full">
           <img src={HeroImage} />
