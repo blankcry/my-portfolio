49d015cc1f8bb3aa21f4c16bdf5572442a835f45
blankcry <gajejames@gmail.com>
2025-06-11 16:20:53 +0100

    chre: final changes

--- src/views/Blogs.tsx
+++ src/views/Blogs.tsx
@@ -1,21 +1,23 @@
 function Blogs() {
   return (
-    <section id="blogs" className="w-full h-screen snap-start flex flex-col gap-8 px-4 md:px-24">
-         <div
-           id="Heading"
-           className="flex flex-col gap-4 w-full text-center justify-center"
-         >
-           <span className="capitalize font-bold text-[30px]">
-             Explore my <span className="gradient-text">Mind.</span>
-           </span>
-         </div>
-         <div className="flex-1 flex text-center w-full justify-center items-center">
-           <span className="capitalize font-bold text-[58px]">
-             Coming<span className="gradient-text"> Soon.</span>
-           </span>
-
-         </div>
-       </section>
+    <section
+      id="blogs"
+      className="w-full h-screen snap-start flex flex-col gap-4 px-4 md:px-14"
+    >
+      <div
+        id="Heading"
+        className="flex flex-col gap-4 w-full text-center justify-center"
+      >
+        <span className="capitalize font-bold text-[20px]">
+          Explore my <span className="gradient-text">Mind.</span>
+        </span>
+      </div>
+      <div className="flex-1 flex text-center w-full justify-center items-center">
+        <span className="capitalize font-bold text-[48px]">
+          Coming<span className="gradient-text"> Soon.</span>
+        </span>
+      </div>
+    </section>
   );
 }
 
